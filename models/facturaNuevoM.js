9a380f095708d93b3f417a05113f7f58483c182d
1Skiy <dbriceno536@gmail.com>
2023-07-16 14:39:04 -0400

    Se agrego la logica de las funciones con la base de datos para facturas

---
+++ models/facturaNuevoM.js
@@ -0,0 +1,65 @@
+const con = require("../database/db")
+
+class facturaModel {
+    Mostrar_todos(){
+        return new Promise((resolve, reject)=>{
+            con.query(
+                "SELECT * FROM factura",
+                function (error, results) {
+                    if (error) {
+
+                        reject(error);
+                    }
+                  resolve(results)
+                }
+            )
+        })
+    }
+
+    Mostrar_por_dia(facturas){
+        return new Promise((resolve, reject)=>{
+            console.log(facturas)
+            con.query(
+                "SELECT * FROM factura WHERE Fecha=?", [facturas.Fecha],
+                function (error, results) {
+                    if (error) {
+
+                        reject(error);
+                    }
+                  resolve(results)
+                }
+            )
+        })
+    }
+
+
+    registro(facturas){
+        return new Promise((resolve, reject)=>{
+            console.log(facturas)
+            con.query("INSERT INTO factura (Nombre, Fecha, Monto) VALUES (?,?,?)",
+            [facturas.Nombre, facturas.Fecha,facturas.Monto],
+            function (error, results, fields) {
+               if (!error) {
+                 resolve(true)
+               } else {
+                reject(error)
+               }
+            })
+        })
+    }
+
+    eliminar(facturas){
+        return new Promise((resolve, reject)=>{
+            console.log(facturas)
+            con.query("DELETE FROM factura WHERE id = ?", [facturas.id], 
+            function (error, results, fields) {
+               if (!error) {
+                 resolve(true)
+               } else {
+                reject(error)
+               }
+            })
+        })
+    }
+}
+module.exports = new facturaModel();
