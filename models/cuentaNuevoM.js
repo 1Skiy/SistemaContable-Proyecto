f69da2e3e63d04fdf7deffc58f01300c277948a8
1Skiy <dbriceno536@gmail.com>
2023-07-16 14:29:47 -0400

    Se agregó en el models de cuenta la logica para las funciones con la base de datos

---
+++ models/cuentaNuevoM.js
@@ -0,0 +1,65 @@
+const con = require("../database/db")
+
+class cuentaModel {
+    Mostrar_todos(){
+        return new Promise((resolve, reject)=>{
+            con.query(
+                "SELECT * FROM cuenta",
+                function (error, results) {
+                    if (error) {
+
+                        reject(error);
+                    }
+                  resolve(results)
+                }
+            )
+        })
+    }
+
+
+    registro(cuentas){
+        return new Promise((resolve, reject)=>{
+            console.log(cuentas)
+            con.query("INSERT INTO cuenta (Nombre, Tipo, Estado) VALUES (?,?,?)",
+            [cuentas.Nombre, cuentas.Tipo,cuentas.Estado],
+            function (error, results, fields) {
+               if (!error) {
+                 resolve(true)
+               } else {
+                reject(error)
+               }
+            })
+        })
+    }
+
+
+    editar(cuentas){
+        return new Promise((resolve, reject)=>{
+            console.log(cuentas)
+            con.query("UPDATE cuenta SET Tipo=?, Estado=? WHERE cuenta.id=?", [cuentas.Tipo, cuentas.Estado, cuentas.id], 
+            function (error, results, fields) {
+               if (!error) {
+                 resolve(true)
+               } else {
+                reject(error)
+               }
+            })
+        })
+    }
+
+    eliminar(cuentas){
+        return new Promise((resolve, reject)=>{
+            console.log(cuentas)
+            con.query("DELETE FROM cuenta WHERE id = ?", [cuentas.id], 
+            function (error, results, fields) {
+               if (!error) {
+                 resolve(true)
+               } else {
+                reject(error)
+               }
+            })
+        })
+    }
+
+}
+module.exports = new cuentaModel();
