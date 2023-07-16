03bb8c82ad55740e29c54146b37b8007a759a729
1Skiy <dbriceno536@gmail.com>
2023-07-16 14:17:15 -0400

    Se elaboró el codigo de mostrar todos, mostrar por cedula y registro de los clientes

---
+++ models/clienteNuevoM.js
@@ -0,0 +1,51 @@
+const con = require("../database/db")
+
+class clienteModel {
+    Mostrar_todos(){
+        return new Promise((resolve, reject)=>{
+            con.query(
+                "SELECT * FROM cliente",
+                function (error, results) {
+                    if (error) {
+
+                        reject(error);
+                    }
+                  resolve(results)
+                }
+            )
+        })
+    }
+
+    Mostrar_por_cedula(clientes){
+        return new Promise((resolve, reject)=>{
+            console.log(clientes)
+            con.query(
+                "SELECT * FROM cliente WHERE Cedula=?", [clientes.Cedula],
+                function (error, results) {
+                    if (error) {
+
+                        reject(error);
+                    }
+                  resolve(results)
+                }
+            )
+        })
+    }
+
+
+    registro(clientes){
+        return new Promise((resolve, reject)=>{
+            console.log(clientes)
+            con.query("INSERT INTO cliente (Nombre, Apellido, Cedula) VALUES (?,?,?)",
+            [clientes.Nombre, clientes.Apellido,clientes.Cedula],
+            function (error, results, fields) {
+               if (!error) {
+                 resolve(true)
+               } else {
+                reject(error)
+               }
+            })
+        })
+    }
+}
+module.exports = new clienteModel();
